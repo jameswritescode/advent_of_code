56388c9ac1f0ed0494bd2d80cd68a2cb15e0ce6b
James Newton <hello@jamesnewton.com>
2017-12-01 11:16:41 -0800

    Update day_1.rb

--- day_1.rb
+++ day_1.rb
@@ -1,8 +1,9 @@
 # step 1 (terminal)
+input = "the captcha".split('').map(&:to_i)
 input.select.with_index { |item, index| item if input[index+1] == item || index == input.size - 1 && input.first == item }.sum
 
 # step 2 (script)
-input = "" # the captcha
+input = "the captcha"
 input = input.split('').map(&:to_i)
 size = input.size
 step = size / 2
