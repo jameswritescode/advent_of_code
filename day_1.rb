cf0c3f1ee8ff5a7313f7ddf911fcbe95addbb448
James Newton <hello@jamesnewton.com>
2017-12-01 16:53:51 -0800

    removed unnessicary size assignment

--- day_1.rb
+++ day_1.rb
@@ -3,10 +3,8 @@ input = "the captcha".split('').map(&:to_i)
 input.select.with_index { |item, index| item if input[index+1] == item || index == input.size - 1 && input.first == item }.sum
 
 # step 2 (script)
-input = "the captcha"
-input = input.split('').map(&:to_i)
-size = input.size
-step = size / 2
+input = "the captcha".split('').map(&:to_i)
+step = input.size / 2
 
 items = input.select.with_index do |item, index|
   step_index = index + step
