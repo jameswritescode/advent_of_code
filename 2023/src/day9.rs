adba0e511c989e5bad376bd3b7076bea06a8a093
James Newton <hello@jamesnewton.com>
2023-12-16 14:13:44 -0800

    2023 day9 part 2

--- 2023/src/day9.rs
+++ 2023/src/day9.rs
@@ -14,8 +14,24 @@ fn differences(numbers: &Vec<i32>) -> Vec<i32> {
     list
 }
 
-fn solve_part1(input: &String) -> i32 {
-    input
+type Stacks = Vec<Vec<Vec<i32>>>;
+
+fn solve_part1(stacks: &Stacks) -> i32 {
+    stacks
+        .iter()
+        .map(|s| s.iter().fold(0, |acc, v| acc + v[v.len() - 1]))
+        .sum()
+}
+
+fn solve_part2(stacks: &Stacks) -> i32 {
+    stacks
+        .iter()
+        .map(|s| s.iter().rev().fold(0, |acc, v| v[0] - acc))
+        .sum()
+}
+
+pub fn run(input: String) {
+    let stacks: Stacks = input
         .lines()
         .map(|l| {
             let list = l
@@ -29,13 +45,13 @@ fn solve_part1(input: &String) -> i32 {
                 stack.push(differences(stack.last().unwrap()));
             }
 
-            stack.iter().fold(0, |acc, v| acc + v.last().unwrap())
+            stack
         })
-        .sum()
-}
+        .collect();
 
-pub fn run(input: String) {
-    let part1_result = solve_part1(&input);
+    let part1_result = solve_part1(&stacks);
+    let part2_result = solve_part2(&stacks);
 
-    println!("Part 1: {part1_result}")
+    println!("Part 1: {part1_result}");
+    println!("Part 2: {part2_result}");
 }
