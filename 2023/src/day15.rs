ed60c7248e3147642953ad5cbd55edc7d25314f1
James Newton <hello@jamesnewton.com>
2023-12-24 17:41:38 -0500

    2023 day15 part 2

--- 2023/src/day15.rs
+++ 2023/src/day15.rs
@@ -1,13 +1,56 @@
-pub fn run(input: String) {
-    let part1_result: u32 = input
-        .trim()
-        .split(',')
-        .map(|chs| {
-            chs.chars()
-                .map(|c| c as u32)
-                .fold(0, |acc, n| ((acc + n) * 17) % 256)
+fn hash(input: &str) -> u32 {
+    input
+        .chars()
+        .map(|c| c as u32)
+        .fold(0, |acc, n| ((acc + n) * 17) % 256)
+}
+
+fn find_label(s: &str) -> &str {
+    &s.split_terminator(&['=', '-'][..]).nth(0).unwrap()
+}
+
+fn solve_part1(seq: &Vec<&str>) -> u32 {
+    seq.iter().map(|chs| hash(&chs)).sum()
+}
+
+fn solve_part2(seq: &Vec<&str>) -> usize {
+    let mut boxes: Vec<Vec<&str>> = vec![vec![]; 256];
+
+    for lens in seq {
+        let label = find_label(&lens);
+        let lenses = &mut boxes[hash(label) as usize];
+        let existing_label_pos = lenses.iter().position(|l| find_label(&l) == label);
+
+        if lens.chars().last().unwrap() == '-' {
+            if let Some(n) = existing_label_pos {
+                lenses.remove(n);
+            }
+        } else if let Some(n) = existing_label_pos {
+            lenses[n] = lens;
+        } else {
+            lenses.push(lens);
+        }
+    }
+
+    boxes
+        .iter()
+        .enumerate()
+        .map(|(i, b)| {
+            b.iter()
+                .enumerate()
+                .map(|(j, c)| {
+                    (i + 1) * (j + 1) * c.chars().last().unwrap().to_digit(10).unwrap() as usize
+                })
+                .sum::<usize>()
         })
-        .sum();
+        .sum()
+}
+
+pub fn run(input: String) {
+    let seq: Vec<_> = input.trim().split(',').collect();
+    let part1_result = solve_part1(&seq);
+    let part2_result = solve_part2(&seq);
 
     println!("Part 1: {}", part1_result);
+    println!("Part 2: {}", part2_result);
 }
