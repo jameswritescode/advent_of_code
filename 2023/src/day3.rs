4667a5f764106d835dbe95ef3220115b01bd8ea2
James Newton <hello@jamesnewton.com>
2023-12-26 13:41:15 -0500

    2023 day3 part 2
    got some ideas from reddit on refactoring this one to achieve part 2

--- 2023/src/day3.rs
+++ 2023/src/day3.rs
@@ -1,106 +1,145 @@
-type GridVector = Vec<Vec<char>>;
+// h/t https://is.gd/rSrqFZ
 
-struct Grid<'a> {
-    rows: usize,
-    cols: usize,
-    vec: &'a GridVector,
+use std::{convert::Infallible, str::FromStr};
+
+#[derive(Debug)]
+struct Part {
+    number: u32,
+    position: usize,
 }
 
-fn valid_part_number(grid: &Grid, row: usize, col: usize) -> bool {
-    let safe_up = row != 0;
-    let safe_down = row != grid.rows - 1;
-    let safe_left = col != 0;
-    let safe_right = col != grid.cols - 1;
+impl Part {
+    fn positions(&self, dimension: usize) -> Vec<usize> {
+        let west = self.position.saturating_sub(1);
+        let east = self.position + self.number.to_string().len();
 
-    let mut positions = vec![];
+        let mut positions = vec![west, east];
 
-    if safe_left {
-        positions.push((row, col - 1));
-    }
+        for n in west..=east {
+            let up = n.saturating_sub(dimension);
 
-    if safe_right {
-        positions.push((row, col + 1));
-    }
+            if up != 0 {
+                positions.push(up);
+            }
 
-    if safe_down {
-        positions.push((row + 1, col));
+            let down = n + dimension;
 
-        if safe_right {
-            positions.push((row + 1, col + 1));
-        }
-        if safe_left {
-            positions.push((row + 1, col - 1));
-        }
-    }
-
-    if safe_up {
-        positions.push((row - 1, col));
-
-        if safe_right {
-            positions.push((row - 1, col + 1));
-        }
-        if safe_left {
-            positions.push((row - 1, col - 1));
-        }
-    }
-
-    positions.iter().any(|(r, c)| {
-        let char = grid.vec[*r][*c];
-
-        char != '.' && !char.is_ascii_digit()
-    })
-}
-
-fn solve_part1(grid: &Grid) -> usize {
-    let mut valid_parts: Vec<usize> = vec![];
-    let mut valid_part_number_found = false;
-
-    for (row, line) in grid.vec.iter().enumerate() {
-        let mut part = String::new();
-
-        for (col, char) in line.iter().enumerate() {
-            if char.is_ascii_digit() {
-                part.push(*char);
-
-                if valid_part_number(&grid, row, col) {
-                    valid_part_number_found = true;
-                }
-            } else if !part.is_empty() {
-                if valid_part_number_found {
-                    valid_parts.push(part.parse().unwrap());
-
-                    valid_part_number_found = false;
-                }
-
-                part.clear()
+            if down < dimension * dimension {
+                positions.push(down);
             }
         }
 
-        if valid_part_number_found {
-            valid_parts.push(part.parse().unwrap());
-            part.clear();
+        positions
+    }
+}
 
-            valid_part_number_found = false;
+#[derive(Debug)]
+struct Schematic {
+    dimension: usize,
+    parts: Vec<Part>,
+    symbols: Vec<char>,
+}
+
+impl FromStr for Schematic {
+    type Err = Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let dimension = s.lines().count();
+        let mut parts = vec![];
+        let mut symbols = vec!['.'; dimension * dimension];
+
+        for (i, row) in s.lines().enumerate() {
+            let mut number_position: Option<usize> = None;
+            let mut number = 0;
+
+            for (j, col) in row.chars().enumerate() {
+                let current_position = j + row.len() * i;
+
+                if col.is_ascii_digit() {
+                    let num = col.to_digit(10).unwrap();
+
+                    if number_position.is_some() {
+                        number = number * 10 + num;
+                    } else {
+                        number_position = Some(current_position);
+                        number = num;
+                    }
+                } else {
+                    if let Some(n) = number_position {
+                        parts.push(Part {
+                            position: n,
+                            number,
+                        });
+
+                        number = 0;
+                        number_position = None;
+                    }
+
+                    symbols[current_position] = col;
+                }
+            }
+
+            if let Some(n) = number_position {
+                parts.push(Part {
+                    position: n,
+                    number,
+                });
+            }
+        }
+
+        Ok(Schematic {
+            parts,
+            symbols,
+            dimension,
+        })
+    }
+}
+
+fn solve_part1(schematic: &Schematic) -> u32 {
+    schematic
+        .parts
+        .iter()
+        .filter_map(|p| {
+            if p.positions(schematic.dimension)
+                .iter()
+                .any(|&p| schematic.symbols[p] != '.')
+            {
+                Some(p.number)
+            } else {
+                None
+            }
+        })
+        .sum()
+}
+
+fn solve_part2(schematic: &Schematic) -> u32 {
+    let mut ratios = vec![vec![]; schematic.symbols.len()];
+
+    for part in &schematic.parts {
+        for pos in part.positions(schematic.dimension) {
+            if schematic.symbols[pos] == '*' {
+                ratios[pos].push(part.number);
+            }
         }
     }
 
-    valid_parts.iter().sum()
+    ratios
+        .iter()
+        .map(|pn| {
+            if pn.len() > 1 {
+                pn.iter().fold(1, |acc, n| acc * n)
+            } else {
+                0
+            }
+        })
+        .sum()
 }
 
 pub fn run(input: String) {
-    let mut vector: GridVector = vec![];
-
-    for line in input.lines() {
-        vector.push(line.chars().collect());
-    }
-
-    let grid = Grid {
-        rows: vector.len(),
-        cols: vector[0].len(),
-        vec: &vector,
-    };
-
-    let part1_result = solve_part1(&grid);
+    let schematic = input.parse::<Schematic>().unwrap();
+    let part1_result = solve_part1(&schematic);
+    let part2_result = solve_part2(&schematic);
 
     println!("Part 1: {part1_result}");
+    println!("Part 2: {part2_result}");
 }
