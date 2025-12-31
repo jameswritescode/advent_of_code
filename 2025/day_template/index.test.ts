8193f7c288866ffd7e22cf11176fe994cf77393d
James Newton <hello@jamesnewton.com>
2025-12-30 18:31:57 -0800

    2025 day template

---
+++ 2025/day_template/index.test.ts
@@ -0,0 +1,14 @@
+import { expect, test } from "vitest";
+
+import day from "./index.ts";
+
+test("sample", () => {
+  const result = day([
+    /* input */
+  ]);
+
+  expect(result).toMatchObject({
+    part1: 0,
+    part2: 0,
+  });
+});
---
+++ 2025/day_template/index.ts
@@ -0,0 +1,10 @@
+import { type Result, solve } from "../helpers.ts";
+
+export default function day(input: string[]): Result {
+  let part1 = 0;
+  let part2 = 0;
+
+  return { part1, part2 };
+}
+
+solve(day);
